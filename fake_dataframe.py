d47c6f788e8825b6e13e6b6812ca921c19a9cf7a
tom4996131207 <tom4996131207@gmail.com>
2018-03-12 11:59:02 +0800

    Create fake_dataframe.py

---
+++ fake_dataframe.py
@@ -0,0 +1,22 @@
+from faker import Faker
+import pandas as pd
+import random
+fake = Faker()
+def create_rows(num=1):
+    output = [{"name":fake.name(),
+               "address":fake.address(),
+               "name":fake.name(),
+               "email":fake.email(),
+               "bs":fake.bs(),
+               "address":fake.address(),
+               "city":fake.city(),
+               "state":fake.state(),
+               "date_time":fake.date_time(),
+               "paragraph":fake.paragraph(),
+               "Conrad":fake.catch_phrase(),
+               "randomdata":random.randint(1000,2000)} for x in range(num)]
+    return output
+
+%%time
+df = pd.DataFrame(create_rows(5000))
+# Wall time: 5.55 s
