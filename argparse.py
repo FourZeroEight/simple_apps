1963be112611d8ef898364371145bd15ea44fd74
FourZeroEight <tom4996131207@gmail.com>
2018-08-13 16:06:07 +0800

    Create argparse.py

---
+++ argparse.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.addadd_argument('--name', default='xxx')
+parser.addadd_argument('--data', default=None)
+args = parser.parse_args()
+
+name = args.name
+if args.daa is None:
+    pass
+else
+    data = args.data
